b18d6d7ef93ee306b09a139715d8480c101059e3
timpactwa <timpactwa@vt.edu>
2026-01-21 23:33:16 -0500

    Inital Commit - Set up some basic scaffolding. Next I will work on setting up postgres and Docker

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => res.json({ ok: true}));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('API running on port ${PORT}'));
